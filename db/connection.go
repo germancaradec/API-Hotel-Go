c4076c2befd20680bb273ae285f779adfc1833a4
germanCaradec <germancaradec@gmail.com>
2024-09-12 12:20:35 -0300

    agregar .env

--- db/connection.go
+++ db/connection.go
@@ -1,29 +1,20 @@
 package db
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+var DSN = "host=localhost user=german password=password dbname=gorm port=5432"
 var DB *gorm.DB
 
 func DBConnection() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, dbname, port)
-
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
+	var error error
+	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	if error != nil {
+		log.Fatal(error)
 	} else {
 		log.Println("Database connection successful")
 	}
